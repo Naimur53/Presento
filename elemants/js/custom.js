1cadc56f4fa9eab3d4b37f1cb164ebf927e176b5
Naimur53 <naimurrhman53@gmail.com>
2021-03-15 11:36:29 +0600

    img hover section

--- elemants/js/custom.js
+++ elemants/js/custom.js
@@ -19,3 +19,15 @@
         prevEl: '.swiper-button-prev',
       },
     });
+    
+
+    //filtering
+    // init Isotope
+var $grid = $('.grid').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
